2595f2ef8eed33a01311c4565dc3bc4c13e71dd5
MrTonyDu <tony@tony.top>
2016-08-31 15:12:24 +0800

    在rewrite前先filter过滤

--- verynginx/lua_script/on_rewrite.lua
+++ verynginx/lua_script/on_rewrite.lua
@@ -1,5 +1,6 @@
 local util = require "util"
 local VeryNginxConfig = require "VeryNginxConfig"
+local filter = require "filter"
 local scheme_lock = require "scheme_lock"
 local redirect = require "redirect"
 local uri_rewrite = require "uri_rewrite"
@@ -12,6 +13,7 @@ end
 --At first , make sure every request use latest running config
 VeryNginxConfig.update_config()
 
+filter.filter()
 scheme_lock.run()
 redirect.run()
 uri_rewrite.run()
